bc754b5d928f4acd2b5cada3b7f7056674382d55
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-19 13:09:34 +0100

    Zip, use 10 replicates

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -6,16 +6,17 @@
 #
 #   ./scripts/rerun.sh
 #
-#
-#SBATCH --time=24:00:00
+#SBATCH --partition=gelifes
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=10G
 #SBATCH --job-name=pirex18
 #SBATCH --output=example_18.log
-module load R
-
+#
 rm -rf example_18
+rm *.png
 time Rscript example_18.R
+zip -r pirouette_example_18.zip example_18 example_18.R scripts *.png
 
